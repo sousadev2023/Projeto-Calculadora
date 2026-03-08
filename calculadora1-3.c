ee09563947c6c346c905081de8e9a579211d747f
sousadev2023 <126911638+sousadev2023@users.noreply.github.com>
2026-03-08 16:43:08 -0300

    Atualização da versão 2.0
    1. Cabeçalho e variáveis
        - foi criado as variaveis (n1 e n2).
        - O scanf foi modificado para ler [1] p/ tabuada e [2] p/ calculo matematico.
    2.Bloco de Tabuada (opção 1)
        - Se for escolhido a opção {1]  o progrmar vai exibir um menu p/  escolha de operações p/
          exibir seu resultador da tabuada.
    3.Bloco de calculadora e raiza (Opção 2)
        - Se opção for igual a [2] o programar vai exibir um menu p/  calculor matemtico aonde consiguira escolhe a operção de calculo a ser feito.

--- calculadora1-3.c
+++ calculadora1-3.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int main(){
-    //calculado 1.1
-    /*Na calculadora 1.1 sera calculado a soma e multiplicação
+    //calculado 1.3
+    /*Na calculadora 1.3 sera calculado a soma, multiplicação, subtracão e divisão.
     do numero digitado pelo teclado*/
     int N, soma = 0, multiplicacao = 0, subtracao, divisao = 0;
     printf("Digite um numero: ");
--- calculadora2-0.c
+++ calculadora2-0.c
@@ -1,21 +1,31 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <stdio.h> // Para ler teclado e exibir texto (printf/scanf).
+#include <string.h>//Responsavel pela manipulção de letras, numeros, simbolos e espaços.
+#include <math.h>// Para funções matemáticas como a raiz quadrada (sqrt).
 
-int main()
-{
-    // calculado 1.1
-    /*Na calculadora 1.1 sera calculado a soma e multiplicação
-    do numero digitado pelo teclado*/
-    int N, soma = 0, multiplicacao = 0, subtracao = 0, divisao = 0;
-    char opcao;
-    printf("Escolha uma opcao\n [+]p/ adicao\n [-] p/ subtracao\n [*]p/ multiplicacao\n [/]p/ divicao\n opcao: ");
-    scanf(" %c", &opcao);
-    printf("\n");
-    printf("Digite um numero: ");
-    scanf("%d", &N);
-    getchar();  
+//1. Cabeçalho e Variáveis
 
+int main(){
+    int soma = 0, multiplicacao = 0, subtracao = 0, divisao = 0;
+    int N;// Usado para o número da tabuada (inteiro)
+    char opcao;// Guarda o caractere digitado (+, -, 1, 2, R, etc)
+    float n1, n2;// Usado na calculadora para aceitar números com vírgula
+
+    printf("Escolha uma Opcao mostra a Tabuada do numero digitado ou fazer um calculo matemtico\n");
+    printf("[1] Tabuada\n[2] Calculo matematico\nOpcao: ");
+    scanf(" %c", &opcao);// O espaço antes do %c limpa o "Enter" da memória
+
+//2. Bloco da Tabuada (Opção 1)
+    if (opcao == '1')
+    {
+        printf("\n------ Menu Tabuada ------\n");
+        printf("Escolha uma opcao\n [+]p/ adicao\n [-] p/ subtracao\n [*]p/ multiplicacao\n [/]p/ divicao\n opcao: ");
+        scanf(" %c", &opcao);
+        printf("\n--------------------------\n");
+
+        printf("\n");
+        printf("Digite um numero: ");
+        scanf("%d", &N);
+        getchar();
         switch (opcao)
         {
         case '+':
@@ -40,18 +50,71 @@ int main()
             }
             break;
         case '/':
-            for (int i = 0; i < 10; i++)
-            {
-                divisao = (i * N) / N;
-                printf("%2d / %2d = %2d\n", i * N, N, divisao);
+            if(N != 0){
+                for (int i = 0; i < 10; i++)
+                {
+                    divisao = (i * N) / N;
+                    printf("%2d / %2d = %2d\n", i * N, N, divisao);
+                }
+            }
+            else{
+                printf("Nao e possivel fazer divisao por zero");
             }
             break;
-        case '1':
-            printf("A raiz quadrada de %d = %.2lf", N, sqrt((double)N));
-            break;
+
         default:
             printf("\nOpcao invalida");
             break;
         }
-        return 0;
     }
+    //3. Bloco da Calculadora e Raiz (Opção 2)
+
+    else if (opcao == '2')
+    {
+        printf("\n----- Menu Calculadora -----\n");
+        printf("Operacao\n [+]p/ adicao\n [-] p/ subtracao\n [*]p/ multiplicacao\n [/]p/ divicao\n [R ou r]p/ Calcula Raiz Quadrada\n opcao: ");
+        scanf(" %c", &opcao);
+        printf("\n----------------------------\n");
+
+        if (opcao == 'R' || opcao == 'r')
+        {
+            printf("Digite um numero: ");
+            scanf("%d", &N);
+            if(N >0){
+                printf("A raiz quadrada de %d = %.2lf", N, sqrt((double)N));
+            }else{
+                printf("\nNumero negativo");
+            }
+            
+        }else{
+            printf("Ditigite um numero: ");
+            scanf("%f", &n1);
+            printf("Digite outro numero: ");
+            scanf("%f", &n2);
+            switch (opcao)
+            {
+            case '+':
+                printf("Resultado = %.2f", n1 + n2);
+                break;
+    
+            case '-':
+                printf("Resultado = %.2f", n1 - n2);
+                break;
+            case '*':
+    
+                printf("Resultado = %.2f", n1 * n2);
+                break;
+    
+            case '/':
+                printf("Resultado = %.2f", n1 / n2);
+                break;
+    
+            default:
+                printf("Opcao invalida!");
+                break;
+            }
+        }
+
+    }
+    return 0;
+}
